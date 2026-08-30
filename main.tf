6affee186a691524cc9840c579b9ee84970840f8
SHAIK ASIF <127130651+shaikasifazeem@users.noreply.github.com>
2026-08-30 15:09:24 +0530

    change

--- main.tf
+++ main.tf
@@ -1,1 +1,59 @@
 
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/src"
+  output_path = "${path.module}/lambda_function.zip"
+}
+
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "${var.function_name}-exec-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# Explicit CloudWatch Log Group with retention limit
+resource "aws_cloudwatch_log_group" "lambda_logs" {
+  name              = "/aws/lambda/${var.function_name}"
+  retention_in_days = 14
+}
+
+# AWS Lambda Function
+resource "aws_lambda_function" "lambda" {
+  function_name    = var.function_name
+  role             = aws_iam_role.lambda_exec.arn
+  handler          = "index.lambda_handler"
+  runtime          = "python3.12"
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+
+  timeout     = 10
+  memory_size = 128
+
+  environment {
+    variables = {
+      ENV = "production"
+    }
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_basic_execution,
+    aws_cloudwatch_log_group.lambda_logs
+  ]
+}
