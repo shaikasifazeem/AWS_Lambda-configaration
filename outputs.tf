c3346ca324b78c30ed19e208de4161140be0454c
SHAIK ASIF <127130651+shaikasifazeem@users.noreply.github.com>
2026-08-30 22:14:22 +0530

    first lambda throttle alarm

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,12 @@ output "lambda_names" {
   value       = [for k, v in aws_lambda_function.lambdas : v.function_name]
   description = "A list of all deployed Lambda function names"
 }
+output "first_lambda_throttle_alarm_arn" {
+  value       = aws_cloudwatch_metric_alarm.first_lambda_throttles.arn
+  description = "The ARN of the first-lambda-function throttle alarm"
+}
+
+output "first_lambda_throttle_alarm_name" {
+  value       = aws_cloudwatch_metric_alarm.first_lambda_throttles.alarm_name
+  description = "The name of the first-lambda-function throttle alarm"
+}
