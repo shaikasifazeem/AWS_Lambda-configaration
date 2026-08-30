70952a040f95faf5817a4fcbbf42099adccbb44a
SHAIK ASIF <127130651+shaikasifazeem@users.noreply.github.com>
2026-08-30 15:11:41 +0530

    outputs
    Add outputs for Lambda function ARN and name

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,9 @@
+output "lambda_arn" {
+  value       = aws_lambda_function.lambda.arn
+  description = "The ARN of the Lambda function"
+}
 
+output "lambda_name" {
+  value       = aws_lambda_function.lambda.function_name
+  description = "The name of the Lambda function"
+}
