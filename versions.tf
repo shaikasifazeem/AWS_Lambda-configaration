43513bd11e2615cc3e3bfeed8f247c64a4598136
SHAIK ASIF <127130651+shaikasifazeem@users.noreply.github.com>
2026-08-30 14:48:11 +0530

    Configure Terraform with AWS

--- versions.tf
+++ versions.tf
@@ -1,1 +1,24 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.4"
+    }
+  }
 
+  backend "s3" {
+    bucket  = "asiftestbucketconfigaration"
+    key     = "lambda/terraform.tfstate"
+    region  = "ap-south-1"  
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
