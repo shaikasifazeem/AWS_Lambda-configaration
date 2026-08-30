1576fd23b78b279b6e38972b07b854c6aa468add
SHAIK ASIF <127130651+shaikasifazeem@users.noreply.github.com>
2026-08-30 22:29:32 +0530

    Lambda invocation alarms

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,13 @@ output "lambda_names" {
   value       = [for k, v in aws_lambda_function.lambdas : v.function_name]
   description = "A list of all deployed Lambda function names"
 }
+
+output "lambda_invocation_alarm_arns" {
+  value       = { for k, v in aws_cloudwatch_metric_alarm.lambda_invocations : k => v.arn }
+  description = "A map of Lambda function names to their invocation alarm ARNs"
+}
+
+output "lambda_invocation_alarm_names" {
+  value       = [for k, v in aws_cloudwatch_metric_alarm.lambda_invocations : v.alarm_name]
+  description = "A list of all Lambda invocation alarm names"
+}
