087892396607e447aee90e4dbb369b48352ebcae
SHAIK ASIF <127130651+shaikasifazeem@users.noreply.github.com>
2026-08-30 22:17:04 +0530

    change
    Adds a CloudWatch Metric Alarm for monitoring throttles on the first lambda function.

--- main.tf
+++ main.tf
@@ -53,3 +53,21 @@ resource "aws_lambda_function" "lambdas" {
     aws_iam_role_policy_attachment.lambda_basic_execution
   ]
 }
+
+# CloudWatch Metric Alarm for first-lambda-function Throttles
+resource "aws_cloudwatch_metric_alarm" "first_lambda_throttles" {
+  alarm_name          = "first-lambda-throttles-alarm"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "Throttles"
+  namespace           = "AWS/Lambda"
+  period              = 60
+  statistic           = "Sum"
+  threshold           = 0
+  alarm_description   = "Alarm triggered when first-lambda-function experiences throttles"
+  treat_missing_data  = "notBreaching"
+
+  dimensions = {
+    FunctionName = aws_lambda_function.lambdas["first-lambda-function"].function_name
+  }
+}
