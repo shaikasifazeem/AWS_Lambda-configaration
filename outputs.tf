305122191c1b235fa4e96dd4d0c51842ff8381d5
SHAIK ASIF <127130651+shaikasifazeem@users.noreply.github.com>
2026-08-31 17:37:23 +0530

    Update Lambda alarm outputs in outputs.tf
    Replaced first_lambda_throttle_alarm_arn output with lambda_throttle_alarm_arns to provide a map of throttle alarm ARNs.

--- outputs.tf
+++ outputs.tf
@@ -8,12 +8,12 @@ output "lambda_names" {
   description = "A list of all deployed Lambda function names"
 }
 
-output "first_lambda_throttle_alarm_arn" {
-  value       = aws_cloudwatch_metric_alarm.first_lambda_throttles.arn
-  description = "The ARN of the first-lambda-function throttle alarm"
+output "lambda_throttle_alarm_arns" {
+  value       = { for k, v in aws_cloudwatch_metric_alarm.lambda_throttles : k => v.arn }
+  description = "A map of Lambda throttle alarm ARNs"
 }
 
 output "lambda_invocation_alarm_arns" {
   value       = { for k, v in aws_cloudwatch_metric_alarm.lambda_invocations : k => v.arn }
-  description = "A map of Lambda function names to their invocation alarm ARNs"
+  description = "A map of Lambda invocation alarm ARNs"
 }
